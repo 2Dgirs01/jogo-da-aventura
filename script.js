e2b4c088f2a218eca76618b3d5721813928c495c
2Dgirs01 <0000113265855xsp@al.educacao.sp.gov.br>
2024-10-02 10:52:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Geovanna Ribeiro Souza do Carmo 2D 18/09/2024 */
+
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
